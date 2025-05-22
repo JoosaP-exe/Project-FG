f64da5ae49d3fe04479f5e98307fbc4548461a30
Valkohattu <ttt.suonpaa@gmail.com>
2025-05-22 09:36:23 +0300

    Dokumenttien fixaaminen ja täydennys (kiitti nääsilä koodista)

--- Code/ProjectFG/obj/Debug/net8.0/ProjectFG.AssemblyInfo.cs
+++ Code/ProjectFG/obj/Debug/net8.0/ProjectFG.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectFG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dfba8ab550df884358fa11e131df5ce6ff5bfa5")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b89adca6f11b8ace376a680a9c0bbf1c6765ecf")]
->>>>>>> origin/main
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b994991e069b39b0a5126355dc72ddb9360de0a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectFG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectFG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
